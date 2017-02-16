2539ec563e3b3f79a1204ab8932dec00e5cc3ee2
Tomas Giedraitis <tomasgiedraitis@gmail.com>
2017-02-16 05:25:02 +0200

    rm unused dotfiles

--- .dotfiles-manager/symlink_dotfiles_xorg.sh
+++ .dotfiles-manager/symlink_dotfiles_xorg.sh
@@ -1,18 +1,10 @@
 #! /bin/bash
 export DOTFILES_DIR=$HOME/.riddle/.dotfiles
 
-ln -sf $DOTFILES_DIR/xorg/.CLion2016.1 $HOME/.CLion2016.1
-ln -sf $DOTFILES_DIR/xorg/.PyCharm2016.2     $HOME/PyCharm2016.2
 ln -sf $DOTFILES_DIR/xorg/.config $HOME/.config
 ln -sf $DOTFILES_DIR/xorg/.conky   $HOME/.conky
-ln -sf $DOTFILES_DIR/xorg/.gimp-2.8      $HOME/.gimp-2.8
 ln -sf $DOTFILES_DIR/xorg/.gmrunrc        $HOME/.gmrunrc
 ln -sf $DOTFILES_DIR/xorg/.gtkrc-2.0  $HOME/.gtkrc-2.0
 ln -sf $DOTFILES_DIR/xorg/.inputrc       $HOME/.inputrc
 ln -sf $DOTFILES_DIR/xorg/.i3   $HOME/.i3
-ln -sf $DOTFILES_DIR/xorg/.kde      $HOME/.kde
-ln -sf $DOTFILES_DIR/xorg/.kde4      $HOME/.kde4
 ln -sf $DOTFILES_DIR/xorg/.local   $HOME/.local
-
-
-
--- .dotfiles-manager/unlink_dotfiles_xorg.sh
+++ .dotfiles-manager/unlink_dotfiles_xorg.sh
@@ -1,15 +1,10 @@
 #! /bin/bash
 
-rm -rf $HOME/.CLion2016.1 
-rm -rf $HOME/.PyCharm2016.2     
 rm -rf $HOME/.config
 rm -rf $HOME/.conky  
-rm -rf $HOME/.gimp-2.8 
 rm -rf $HOME/.gmrunrc  
 rm -rf $HOME/.gtkrc-2.0  
 rm -rf $HOME/.inputrc  
 rm -rf $HOME/.i3   
-rm -rf $HOME/.kde      
-rm -rf $HOME/.kde4     
 rm -rf $HOME/.local  
 
