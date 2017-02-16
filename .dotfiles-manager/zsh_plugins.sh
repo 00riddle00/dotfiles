1a151c996bc1f6a7b1a4986c893006456c39e345
Tomas Giedraitis <tomasgiedraitis@gmail.com>
2017-02-16 05:46:48 +0200

    modify vim and zsh plugin start scripts

--- .dotfiles-manager/zsh_plugins.sh
+++ .dotfiles-manager/zsh_plugins.sh
@@ -1,6 +1,2 @@
 #! /bin/bash
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" &&
-rm -f $DOTFILES_DIR/cmd/.oh-my-zsh/.gitkeep &&
-#mv $HOME/.oh-my-zsh/ $DOTFILES_DIR/cmd/ &&
-ln -sf $DOTFILES_DIR/cmd/.oh-my-zsh $HOME/.oh-my-zsh &&
-ln -sf $DOTFILES_DIR/cmd/.zshrc $HOME/.zshrc
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" 
