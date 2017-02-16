40a922afc7a14232f6556a1a960e2e35662c23dc
Tomas Giedraitis <tomasgiedraitis@gmail.com>
2017-02-16 23:56:07 +0200

    jetbrains dotfiles

--- .dotfiles-manager/symlink_dotfiles_xorg.sh
+++ .dotfiles-manager/symlink_dotfiles_xorg.sh
@@ -9,10 +9,10 @@ ln -sf $DOTFILES_DIR/xorg/.inputrc       $HOME/.inputrc
 ln -sf $DOTFILES_DIR/xorg/.i3   $HOME/.i3
 ln -sf $DOTFILES_DIR/xorg/.local   $HOME/.local
 
-ln -sf $DOTFILES_DIR/xorg/.CLion2016.1 $HOME/.CLion2016.1 
-ln -sf $DOTFILES_DIR/xorg/.PyCharm2016.2   $HOME/.PyCharm2016.2
+ln -sf $DOTFILES_DIR/xorg/.CLion2016.3 $HOME/.CLion2016.3 
+ln -sf $DOTFILES_DIR/xorg/.PyCharm2016.3   $HOME/.PyCharm2016.3
 ln -sf $DOTFILES_DIR/xorg/.gimp-2.8   $HOME/.gimp-2.8
-ln -sf $DOTFILES_DIR/xorg/.kde4   $HOME/.kde4
+ln -sf $DOTFILES_DIR/xorg/.kde   $HOME/.kde
 ln -sf $DOTFILES_DIR/xorg/.kde4   $HOME/.kde4
 ln -sf $DOTFILES_DIR/xorg/.mozilla   $HOME/.mozilla
 
--- .dotfiles-manager/unlink_dotfiles_xorg.sh
+++ .dotfiles-manager/unlink_dotfiles_xorg.sh
@@ -7,8 +7,8 @@ rm -rf $HOME/.gtkrc-2.0
 rm -rf $HOME/.inputrc  
 rm -rf $HOME/.i3   
 rm -rf $HOME/.local   
-rm -rf $HOME/.CLion2016.1  
-rm -rf $HOME/.PyCharm2016.2   
+rm -rf $HOME/.CLion2016.3
+rm -rf $HOME/.PyCharm2016.3
 rm -rf $HOME/.gimp-2.8   
 rm -rf $HOME/.kde
 rm -rf $HOME/.kde4
