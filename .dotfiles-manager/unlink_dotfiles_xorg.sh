243acfb0b090410c738a4493505b732175c82e50
Tomas Giedraitis <tomasgiedraitis@gmail.com>
2017-02-16 23:31:23 +0200

    dotfiles manager files

--- .dotfiles-manager/unlink_dotfiles_xorg.sh
+++ .dotfiles-manager/unlink_dotfiles_xorg.sh
@@ -6,5 +6,11 @@ rm -rf $HOME/.gmrunrc
 rm -rf $HOME/.gtkrc-2.0  
 rm -rf $HOME/.inputrc  
 rm -rf $HOME/.i3   
-rm -rf $HOME/.local  
+rm -rf $HOME/.local   
+rm -rf $HOME/.CLion2016.1  
+rm -rf $HOME/.PyCharm2016.2   
+rm -rf $HOME/.gimp-2.8   
+rm -rf $HOME/.kde
+rm -rf $HOME/.kde4
+rm -rf $HOME/.mozilla
 
