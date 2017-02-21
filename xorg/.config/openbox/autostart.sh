45b078f21bc4abebc443e91873c28d731f782c65
Tomas Giedraitis <tomasgiedraitis@gmail.com>
2017-02-21 22:07:42 +0200

    config upddate

--- xorg/.config/openbox/autostart.sh
+++ xorg/.config/openbox/autostart.sh
@@ -14,7 +14,7 @@
 
 
 # HDMI left eDP  (HOME)
-xrandr --output HDMI-1 --auto --output eDP-1 --auto --right-of HDMI-1
+xrandr --output HDMI-1 --auto #--output eDP-1 --auto --right-of HDMI-1
 
 # OSOS 2 screens atomik
 # xrandr --output HDMI-1 --auto --output DVI-D-1 --auto --right-of HDMI-1
