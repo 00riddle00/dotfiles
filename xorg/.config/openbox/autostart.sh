0e78a0bcfeb6da223243e1916e9c6debe3c1c6ad
Tomas Giedraitis <tomasgiedraitis@gmail.com>
2017-02-22 16:19:19 +0200

    vim config

--- xorg/.config/openbox/autostart.sh
+++ xorg/.config/openbox/autostart.sh
@@ -14,7 +14,8 @@
 
 
 # HDMI left eDP  (HOME)
-xrandr --output HDMI-1 --auto --output eDP-1 --auto --right-of HDMI-1
+xrandr --output HDMI-1 --auto #--output eDP-1 --auto --right-of HDMI-1
+xrandr --output HDMI-1 --auto 
 
 # OSOS 2 screens atomik
 # xrandr --output HDMI-1 --auto --output DVI-D-1 --auto --right-of HDMI-1
@@ -28,6 +29,5 @@ xrandr --output HDMI-1 --auto --output eDP-1 --auto --right-of HDMI-1
  #xrandr --output HDMI-1 --auto --rotate left --output eDP-1 --auto --right-of HDMI-1
 
 #autokey-gtk &
-gnome-screensaver &
 feh --bg-scale $CANDY/wallpapers/black_arch.jpg &
-conky -c $DOTFILES_DIR/xorg/.conky/archconky &
+#conky -c $DOTFILES_DIR/xorg/.conky/archconky &
