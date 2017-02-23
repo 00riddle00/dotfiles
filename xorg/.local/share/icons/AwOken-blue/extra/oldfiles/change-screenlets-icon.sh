17dcffc757595ab6845e7069afc050789bc1e6ea
Tomas Giedraitis <tomasgiedraitis@gmail.com>
2017-02-23 19:30:31 +0200

    rm themes and icons

--- xorg/.local/share/icons/AwOken-blue/extra/oldfiles/change-screenlets-icon.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo Please provide root password
-
-sudo mv /usr/share/icons/hicolor/scalable/apps/screenlets.svg /usr/share/icons/hicolor/scalable/apps/screenlets-backup.svg
-sudo cp extra/screenlets/screenlets.png /usr/share/icons/hicolor/scalable/apps
-
-echo DONE
