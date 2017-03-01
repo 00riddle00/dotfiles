2a4b53ce274d48f47165209c5690c9bdeb8c4836
Tomas Giedraitis <tomasgiedraitis@gmail.com>
2017-03-01 14:05:37 +0100

    add dotfiles_dir in script

--- .dotfiles-manager/vim_plugins.sh
+++ .dotfiles-manager/vim_plugins.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
+export DOTFILES_DIR=/home/riddle/.riddle/.dotfiles
+
 rm -f $DOTFILES_DIR/cmd/.vim/bundle/.gitkeep &&
 git clone https://github.com/VundleVim/Vundle.vim.git $DOTFILES_DIR/cmd/.vim/bundle/Vundle.vim &&
 touch $DOTFILES_DIR/cmd/.vim/bundle/.gitkeep &&
