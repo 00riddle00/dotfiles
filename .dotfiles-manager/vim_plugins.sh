b5421f22236f11638e925521d8237a69df53c035
Tomas Giedraitis <tomasgiedraitis@gmail.com>
2017-05-04 22:24:06 +0300

    .

--- .dotfiles-manager/vim_plugins.sh
+++ .dotfiles-manager/vim_plugins.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 export DOTFILES_DIR=/home/riddle/.riddle/.dotfiles
 
+# TODO remove gitkeep
+
 rm -f $DOTFILES_DIR/cmd/.vim/bundle/.gitkeep &&
 git clone https://github.com/VundleVim/Vundle.vim.git $DOTFILES_DIR/cmd/.vim/bundle/Vundle.vim &&
 touch $DOTFILES_DIR/cmd/.vim/bundle/.gitkeep &&
