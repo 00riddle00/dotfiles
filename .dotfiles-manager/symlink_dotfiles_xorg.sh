15b04c7654d11068896283cfd62b9a0b07561bc7
Tomas Giedraitis <tomasgiedraitis@gmail.com>
2017-05-05 12:19:45 +0300

    .

--- .dotfiles-manager/symlink_dotfiles_xorg.sh
+++ .dotfiles-manager/symlink_dotfiles_xorg.sh
@@ -1,19 +1,16 @@
 #! /bin/bash
 export DOTFILES_DIR=/home/riddle/.riddle/.dotfiles
 
+ln -sf $DOTFILES_DIR/xorg/.atom   $HOME/.atom
 ln -sf $DOTFILES_DIR/xorg/.config $HOME/.config
 ln -sf $DOTFILES_DIR/xorg/.conky   $HOME/.conky
+ln -sf $DOTFILES_DIR/xorg/.gimp-2.8   $HOME/.gimp-2.8
 ln -sf $DOTFILES_DIR/xorg/.gmrunrc        $HOME/.gmrunrc
 ln -sf $DOTFILES_DIR/xorg/.gtkrc-2.0  $HOME/.gtkrc-2.0
-ln -sf $DOTFILES_DIR/xorg/.inputrc       $HOME/.inputrc
+ln -sf $DOTFILES_DIR/xorg/.gtkrc-2.0.mine  $HOME/.gtkrc-2.0.mine
 ln -sf $DOTFILES_DIR/xorg/.i3   $HOME/.i3
-ln -sf $DOTFILES_DIR/xorg/.local   $HOME/.local
-ln -sf $DOTFILES_DIR/xorg/.gimp-2.8   $HOME/.gimp-2.8
+ln -sf $DOTFILES_DIR/xorg/.inputrc       $HOME/.inputrc
+ln -sf $DOTFILES_DIR/xorg/.jetbrains.custom_folder   $HOME/.jetbrains.custom_folder 
 ln -sf $DOTFILES_DIR/xorg/.kde   $HOME/.kde
 ln -sf $DOTFILES_DIR/xorg/.kde4   $HOME/.kde4
-ln -sf $DOTFILES_DIR/xorg/.mplayer   $HOME/.mplayer
-ln -sf $DOTFILES_DIR/xorg/.xmonad   $HOME/.xmonad
-ln -sf $DOTFILES_DIR/xorg/.atom   $HOME/.atom
-
-
 
--- xorg/.config/openbox/autostart.sh
+++ xorg/.config/openbox/autostart.sh
@@ -1,1 +1,17 @@
-/home/riddle/.riddle/.shell-scripts/themes/riddle/autostart.sh
+#! /bin/sh
+
+xrandr --output HDMI-1-1 --auto 
+
+#autokey-gtk &
+feh --bg-scale $CANDY/wallpapers/black_arch_2.jpg &
+gnome-screensaver > /dev/null 2>&1 &
+
+ps cax | grep conky > /dev/null 2>&1 
+if [ $? -eq 0 ]; then
+    :
+else
+	conky -c $DOTFILES_DIR/xorg/.conky/clock_blue > /dev/null 2>&1 &
+	conky -c $DOTFILES_DIR/xorg/.conky/stats_blue > /dev/null 2>&1 &
+fi
+
+
