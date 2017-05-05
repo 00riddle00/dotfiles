7c073b9d147379e16e1d8ac7fe479016981335b1
Tomas Giedraitis <tomasgiedraitis@gmail.com>
2017-05-05 12:24:57 +0300

    .

--- .dotfiles-manager/zsh_plugins.sh
+++ .dotfiles-manager/zsh_plugins.sh
@@ -3,5 +3,7 @@
 export DOTFILES_DIR=/home/riddle/.riddle/.dotfiles
 
 rm -rf $HOME/.oh-my-zsh && 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" &&
-ln -sf $DOTFILES_DIR/cmd/.zshrc $HOME/.zshrc
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" 
+
+# You should execute this command after that: (since oh-my-zsh install recreates .zshrc file anew)
+# ln -sf $DOTFILES_DIR/cmd/.zshrc $HOME/.zshrc
