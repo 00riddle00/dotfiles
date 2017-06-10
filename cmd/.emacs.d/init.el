4701de10b57a5bfed7052f5dc53485adf7e4deb4
Tomas Giedraitis <tomasgiedraitis@gmail.com>
2017-06-10 22:05:12 +0300

    .

--- cmd/.emacs.d/init.el
+++ cmd/.emacs.d/init.el
@@ -9,12 +9,16 @@
    ["black" "#d55e00" "#009e73" "#f8ec59" "#0072b2" "#cc79a7" "#56b4e9" "white"])
  '(custom-enabled-themes (quote (deeper-blue)))
  '(custom-unlispify-menu-entries nil)
- '(tool-bar-mode nil)
+ '(inhibit-startup-screen t)
+ '(nil nil t)
+ '(package-selected-packages (quote (evil)))
  '(scroll-bar-mode -1)
- ;;'(load-theme 'solarized-dark')
- ;;'(load-theme 'wombat')
- (disable-theme 'wombat)
- '(inhibit-startup-screen t))
+ '(tool-bar-mode nil))
+  (require 'package)
+  (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+  (package-initialize)
+  (require 'evil)
+  (evil-mode 1)
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
