885f7b6cc24b09588e40f6a6de95473dfe46a446
Tomas Giedraitis <tomasgiedraitis@gmail.com>
2017-06-12 12:20:42 +0300

    .

--- cmd/.emacs.d/init.el
+++ cmd/.emacs.d/init.el
@@ -19,9 +19,13 @@
   (package-initialize)
   (require 'evil)
   (evil-mode 1)
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+  (setq package-archives
+  '(("gnu" . "http://elpa.gnu.org/packages/")
+  ("marmalade" . "http://marmalade-repo.org/packages/")
+  ("melpa" . "http://melpa.milkbox.net/packages/")))
+  (custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+  )
