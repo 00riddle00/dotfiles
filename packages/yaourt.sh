a822202b6bf87f5426dc811cfffe85503d700856
Tomas Giedraitis <tomasgiedraitis@gmail.com>
2017-06-28 22:35:39 +0300

    .

---
+++ packages/yaourt.sh
@@ -0,0 +1,98 @@
+This file contains packages I use on Linux and install with Pacman/yaourt.
+Install them with the following command.
+
+  for PKG in $(sed '1,14d' ~/.yaourt); do
+    yaourt -S --noconfirm $PKG||echo $PKG failed.
+  done
+
+To sort the list when adding packages, use the following command.
+
+ head -n 14 ~/.yaourt > /tmp/.yaourt-new
+ sed '1,14d' ~/.yaourt | sort >> /tmp/.yaourt-new
+ cat /tmp/.yaourt-new > .yaourt
+
+
+android-ndk
+android-sdk
+android-sdk-platform-tools
+android-udev
+anki
+archey3
+aria2
+autoconf
+baobab
+cheese
+chromium
+chromium-libpdf-stable
+chromium-pepper-flash-stable
+cloudprint-cups-git
+conky
+cups
+dmenu
+dmenu-aliases
+docker
+downgrade
+dunst
+emacs
+fabric
+feh
+gcc
+gcc-fortran-multilib
+gcc-libs
+gcc-multilib
+gdb
+ghc
+git
+google-talkplugin
+gvim
+gzip
+hplip
+i3lock
+i3status
+i3-wm
+libnotify
+make
+mosh
+mpv
+mutt
+nmap
+openssh
+pass
+recordmydesktop
+rfkill
+rsync
+rxvt-unicode
+sbt
+scala
+screen
+scrot
+sdcv
+sed
+slock
+sshfs
+stardict-oald
+stardict-wordnet
+strace
+subversion
+tar
+the_silver_searcher
+tor-browser-en
+torsocks
+tree
+unrar
+unzip
+urxvt-perls
+urxvt-font-size-git
+valgrind
+wget
+wireshark-gtk
+xclip
+xournal
+xterm
+yaourt
+youtube-dl
+zathura
+zathura-pdf-poppler
+zathura-ps
+zip
+zsh
--- xorg/.config/openbox/autostart.sh
+++ xorg/.config/openbox/autostart.sh
@@ -1,2 +1,12 @@
 #! /bin/sh
+xrandr --output HDMI-1-1 --auto &&
+conky -c $DOTFILES_DIR/xorg/.conky/stats_blue > /dev/null 2>&1 &
+conky -c $DOTFILES_DIR/xorg/.conky/clock_blue > /dev/null 2>&1 &
 xrdb ~/.Xresources && urxvt
+
+
+
+#xrdb ~/.Xresources && urxvt
+#conky -c $DOTFILES_DIR/xorg/.conky/clock_blue > /dev/null 2>&1  &&
+#conky -c $DOTFILES_DIR/xorg/.conky/stats_blue > /dev/null 2>&1 &
+
