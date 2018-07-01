c31e7012bf08d285317b005fcef9847120bdaa49
Tomas Giedraitis <tomasgiedraitis@gmail.com>
2018-07-01 14:31:33 +0300

    .

--- xorg/.config/cmus/lib.pl
+++
@@ -1,5 +0,0 @@
-/home/riddle/.riddle/.riddle-candy/startup_sounds/matrix.mp3
-/home/riddle/.riddle/.riddle-candy/startup_sounds/waiting.mp3
-/home/riddle/.riddle/.riddle-candy/startup_sounds/waiting_at_last.mp3
-/home/riddle/.riddle/.riddle-candy/startup_sounds/waiting_at_last_march.mp3
-/home/riddle/.riddle/.riddle-candy/startup_sounds/waiting_march.mp3
