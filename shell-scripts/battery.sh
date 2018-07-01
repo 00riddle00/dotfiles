e2746717ad264b1090445d150b4c31cd43109f2d
Tomas Giedraitis <tomasgiedraitis@gmail.com>
2018-07-01 13:35:47 +0300

    .

---
+++ shell-scripts/battery.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# A script to test whether a laptop is charging
+cmd="upower -i `upower -e | grep 'BAT'` > /dev/null 2>&1"
+
+if $cmd | grep -i "state" | grep -i "dis" > /dev/null 2>&1; then
+    echo "-----"
+    espeak "Your laptop needs a charger" > /dev/null 2>&1
+else
+    echo "+++++"
+    espeak "Your laptop is charging" > /dev/null 2>&1
+fi
--- xorg/.config/openbox/autostart.sh
+++ xorg/.config/openbox/autostart.sh
@@ -4,5 +4,8 @@ conky -c $DOTFILES_DIR/xorg/.conky/stats_blue > /dev/null 2>&1 &
 conky -c $DOTFILES_DIR/xorg/.conky/clock_blue > /dev/null 2>&1 &
 xrdb ~/.Xresources && 
 feh  --bg-scale "$MAIN_HOME/Dropbox/sync/candy/wallpapers/black_arch_2.jpg" && 
-terminator --geometry=700x500 -T t2
+espeak "Welcome home, Riddle!" > /dev/null 2>&1  &&
+terminator --geometry=700x500 -T t2 
+#mpg123 $CANDY/startup_sounds/star_wars.mp3 > /dev/null 2>&1 &&
+#terminator --geometry=700x500 -T t2 -e cmatrix 
 #urxvt -name t2
