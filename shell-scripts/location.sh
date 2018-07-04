5a0ca72c3eb0d8150902bade580c89f13c2a2164
Tomas Giedraitis <tomasgiedraitis@gmail.com>
2018-07-04 17:03:41 +0300

    .

---
+++ shell-scripts/location.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+xdotool key f;
+xdotool sleep 1
+xdotool key i;
+xdotool sleep 1
+xdotool key f;
+xdotool sleep 1
+xdotool key j;
+xdotool sleep 2
+xdotool key Return;
+xdotool sleep 3
+
