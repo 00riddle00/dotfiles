529d7b8c5a766934a3e1dd11f1668bc939df3d7b
Tomas Giedraitis <tomasgiedraitis@gmail.com>
2019-04-03 00:15:26 +0300

    .

--- xorg/.config/openbox/autostart.sh
+++ xorg/.config/openbox/autostart.sh
@@ -4,10 +4,10 @@ conky -c $DOTFILES_DIR/xorg/.conky/stats_blue > /dev/null 2>&1 &
 conky -c $DOTFILES_DIR/xorg/.conky/clock_blue_full > /dev/null 2>&1 &
 conky -c $DOTFILES_DIR/xorg/.conky/inline_stats_blue > /dev/null 2>&1 &
 xrdb ~/.Xresources && 
-feh  --bg-scale "$MAIN_HOME/Dropbox/sync/candy/wallpapers/black_arch_1.jpg" && 
+feh  --bg-scale "$MAIN_HOME/Dropbox/sync/candy/wallpapers/solarized.jpg" && 
 espeak "Welcome home, Riddle!" > /dev/null 2>&1  &&
 urxvt -e cmatrix &
 mpg123 $CANDY/startup_sounds/star_wars.mp3 > /dev/null 2>&1 &&
-feh  --bg-scale "$MAIN_HOME/Dropbox/sync/candy/wallpapers/black_arch_2.jpg" && 
+feh  --bg-scale "$MAIN_HOME/Dropbox/sync/candy/wallpapers/solarized.jpg" && 
 killall urxvt &&
 urxvt -name t2
