cb037e7f390088565cd22ac684da745a8e69564b
Tomas Giedraitis <tomasgiedraitis@gmail.com>
2019-09-14 09:36:02 +0300

    .

--- packages/archlinux/update_package_data.sh
+++ packages/archlinux/update_package_data.sh
@@ -4,3 +4,4 @@ pacman -Qqm > packages_aur
 pacman -Qqe > pacman.lst
 # palieka tik tas eilutes, kurios nematchina su pattern_file (packages_aur)
 grep -Fvx -f packages_aur pacman.lst > packages_main_repos
+rm pacman.lst
