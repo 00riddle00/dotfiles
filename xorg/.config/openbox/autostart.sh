d2ae8eb555f81196160402703fb96d2bd08e6ce4
Tomas Giedraitis <tomasgiedraitis@gmail.com>
2019-10-19 11:50:32 +0300

    Add numbering function to .aliasies

--- xorg/.config/openbox/autostart.sh
+++ xorg/.config/openbox/autostart.sh
@@ -2,12 +2,21 @@
 xrandr --output HDMI-1-1 --auto &&
 conky -c $DOTFILES_DIR/xorg/.conky/stats_blue > /dev/null 2>&1 &
 conky -c $DOTFILES_DIR/xorg/.conky/clock_blue_full > /dev/null 2>&1 &
-conky -c $DOTFILES_DIR/xorg/.conky/inline_stats_blue > /dev/null 2>&1 &
 xrdb ~/.Xresources && 
 feh  --bg-scale "$MAIN_HOME/Dropbox/sync/candy/wallpapers/arch5.jpg" && 
-espeak "Welcome home, Riddle!" > /dev/null 2>&1  &&
-urxvt -e cmatrix &
-mpg123 $CANDY/startup_sounds/star_wars.mp3 > /dev/null 2>&1 &&
-feh  --bg-scale "$MAIN_HOME/Dropbox/sync/candy/wallpapers/arch5.jpg" && 
-killall urxvt &&
-urxvt -name t2
+urxvt -name t2 &&
+tint2
+
+# previous fancy start
+#xrandr --output HDMI-1-1 --auto &&
+#conky -c $DOTFILES_DIR/xorg/.conky/stats_blue > /dev/null 2>&1 &
+#conky -c $DOTFILES_DIR/xorg/.conky/clock_blue_full > /dev/null 2>&1 &
+#conky -c $DOTFILES_DIR/xorg/.conky/inline_stats_blue > /dev/null 2>&1 &
+#xrdb ~/.Xresources && 
+#feh  --bg-scale "$MAIN_HOME/Dropbox/sync/candy/wallpapers/arch5.jpg" && 
+#espeak "Welcome home, Riddle!" > /dev/null 2>&1  &&
+#urxvt -e cmatrix &
+#mpg123 $CANDY/startup_sounds/star_wars.mp3 > /dev/null 2>&1 &&
+#feh  --bg-scale "$MAIN_HOME/Dropbox/sync/candy/wallpapers/arch5.jpg" && 
+#killall urxvt &&
+#urxvt -name t2
