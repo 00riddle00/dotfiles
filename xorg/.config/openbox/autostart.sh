7d98d807d2ea484a94c50308ddbbb0913deb4d5f
Tomas Giedraitis <tomasgiedraitis@gmail.com>
2019-12-21 20:42:26 +0200

    .

--- xorg/.config/openbox/autostart.sh
+++ xorg/.config/openbox/autostart.sh
@@ -1,8 +1,15 @@
 #! /bin/sh
+#xset -b
+#xrandr --output HDMI-1-1 --auto &&
+#(sleep 2s && feh  --bg-scale "$MAIN_HOME/Dropbox/sync/candy/wallpapers/arch5.jpg") & 
+#(sleep 2s && conky -c $DOTFILES_DIR/xorg/.conky/stats_blue > /dev/null 2>&1) &
+#(sleep 2s && conky -c $DOTFILES_DIR/xorg/.conky/clock_blue_full > /dev/null 2>&1) &
+#(sleep 2s && urxvt -name t2) &
+#tint2
+
 xrandr --output HDMI-1-1 --auto &&
 conky -c $DOTFILES_DIR/xorg/.conky/stats_blue > /dev/null 2>&1 &
 conky -c $DOTFILES_DIR/xorg/.conky/clock_blue_full > /dev/null 2>&1 &
-xrdb ~/.Xresources && 
 feh  --bg-scale "$MAIN_HOME/Dropbox/sync/candy/wallpapers/arch5.jpg" && 
 urxvt -name t2 &&
 tint2
