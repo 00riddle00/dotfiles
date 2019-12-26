175a469fe653d478989e69c533200e604b9af429
Tomas Giedraitis <tomasgiedraitis@gmail.com>
2019-12-26 18:01:03 +0200

    .

--- xorg/.config/openbox/autostart.sh
+++ xorg/.config/openbox/autostart.sh
@@ -1,29 +1,28 @@
 #! /bin/sh
-#xset -b
-#xrandr --output HDMI-1-1 --auto &&
-#(sleep 2s && feh  --bg-scale "$MAIN_HOME/Dropbox/sync/candy/wallpapers/arch5.jpg") & 
-#(sleep 2s && conky -c $DOTFILES_DIR/xorg/.conky/stats_blue > /dev/null 2>&1) &
-#(sleep 2s && conky -c $DOTFILES_DIR/xorg/.conky/clock_blue_full > /dev/null 2>&1) &
-#(sleep 2s && urxvt -name t2) &
-#tint2
 
-xrandr --output HDMI-1-1 --auto &&
+export HDMI_SCREEN=HDMI-1-1
+export LAPTOP_SCREEN=eDP-1-1
+
+## check if X is running
+if xset q &>/dev/null; then
+
+    ## Configure display
+    ## if HDMI display connected, use it. Else use eDP.
+    xrandr | awk '/ connected/{print $1}' | grep $HDMI_SCREEN > /dev/null 2>&1
+    if [ $? -eq 0 ]; then
+        xrandr --output $HDMI_SCREEN --auto
+        :
+    else
+        xrandr --output $LAPTOP_SCREEN --auto
+    fi;
+fi;
+
+feh  --bg-scale "$MAIN_HOME/Dropbox/sync/candy/wallpapers/arch5.jpg" && 
 conky -c $DOTFILES_DIR/xorg/.conky/stats_blue > /dev/null 2>&1 &
 conky -c $DOTFILES_DIR/xorg/.conky/clock_blue_full > /dev/null 2>&1 &
-feh  --bg-scale "$MAIN_HOME/Dropbox/sync/candy/wallpapers/arch5.jpg" && 
-urxvt -name t2 &&
-tint2
+# espeak "Welcome home, Riddle!" > /dev/null 2>&1  &&
+# mpg123 $CANDY/startup_sounds/star_wars.mp3 > /dev/null 2>&1 &
+tint2 &
+# timeout 30s urxvt -name matrix -e cmatrix
+urxvt -name t2
 
-# previous fancy start
-#xrandr --output HDMI-1-1 --auto &&
-#conky -c $DOTFILES_DIR/xorg/.conky/stats_blue > /dev/null 2>&1 &
-#conky -c $DOTFILES_DIR/xorg/.conky/clock_blue_full > /dev/null 2>&1 &
-#conky -c $DOTFILES_DIR/xorg/.conky/inline_stats_blue > /dev/null 2>&1 &
-#xrdb ~/.Xresources && 
-#feh  --bg-scale "$MAIN_HOME/Dropbox/sync/candy/wallpapers/arch5.jpg" && 
-#espeak "Welcome home, Riddle!" > /dev/null 2>&1  &&
-#urxvt -e cmatrix &
-#mpg123 $CANDY/startup_sounds/star_wars.mp3 > /dev/null 2>&1 &&
-#feh  --bg-scale "$MAIN_HOME/Dropbox/sync/candy/wallpapers/arch5.jpg" && 
-#killall urxvt &&
-#urxvt -name t2
