aae9ca896b59e951dcd4321548d3586de36b1fc8
Tomas Giedraitis <tomasgiedraitis@gmail.com>
2020-03-27 23:42:09 +0200

    [1] start qutebrowser config [2] qutebrowser default page with ever changing image

---
+++ .config/qutebrowser/config.py
@@ -0,0 +1,16 @@
+
+# general settings
+c.url.default_page = 'file:///home/riddle/.local/bin/share/start-page/index.html'
+c.url.start_pages = 'file:///home/riddle/.local/bin/share/start-page/index.html'
+
+# searches
+c.url.searchengines['qw'] = 'https://www.qwant.com/?q={}'
+c.url.searchengines['aw'] = 'https://wiki.archlinux.org/?search={}'
+c.url.searchengines['d'] = 'https://duckduckgo.com/?q={}'
+c.url.searchengines['g'] = 'https://www.google.com/search?hl=en&q={}'
+c.url.searchengines['yt'] = 'https://youtube.com/results?search_query={}'
+c.url.searchengines['w'] = 'https://en.wikipedia.org/?search={}'
+c.url.searchengines['gh'] = 'https://github.com/search?q={}&type=Code'
+c.url.searchengines['ap'] = 'https://www.archlinux.org/packages/?sort=&q={}'
+c.url.searchengines['rd'] = 'https://reddit.com/r/{}'
+c.url.searchengines['DEFAULT'] = c.url.searchengines['d']
