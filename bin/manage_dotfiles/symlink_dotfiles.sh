528b5d1b18d256a7904759d731d948b822618252
Tomas Giedraitis <tomasgiedraitis@gmail.com>
2020-03-28 21:08:39 +0200

    [1] dotfiles management shell scripts edit (xdg base dir specs) [2] mimetypes

--- bin/manage_dotfiles/symlink_dotfiles.sh
+++ bin/manage_dotfiles/symlink_dotfiles.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+# $HOME dir
 for entry in $DOTFILES_DIR/.[a-zA-Z]*; do
     # whenever you iterate over files/folders by globbing, it's good practice to avoid the corner 
     # case where the glob does not match (which makes the loop variable expand to the 
@@ -7,21 +8,24 @@ for entry in $DOTFILES_DIR/.[a-zA-Z]*; do
     [ -e "$entry" ] && 
         [[ ${entry##*/} != ".config" ]] &&
         [[ ${entry##*/} != ".local" ]] &&
-        # .gitconfig is also ignored. Will deal with that later
         [[ ${entry##*/} != .git* ]] &&
     ln -sf $entry $HOME/${entry##*/}
 done
 
-# mkdir -p $HOME/.config
-
+# $XDG_CONFIG_HOME dir
 for entry in $DOTFILES_DIR/.config/[a-zA-Z]*; do
     [ -e "$entry" ] &&
-    ln -sf $entry $HOME/.config/${entry##*/}
+    ln -sf $entry $XDG_CONFIG_HOME/${entry##*/}
 done
 
+# $XDG_DATA_HOME dir
 local_bin="$DOTFILES_DIR/.local/bin"
 [ -e "$local_bin" ] && ln -sf $local_bin $HOME/.local/bin
 
 local_shared="$DOTFILES_DIR/.local/share/riddle00"
-[ -e "$local_shared" ] && ln -sf $local_shared $HOME/.local/share/riddle00
+[ -e "$local_shared" ] && ln -sf $local_shared $XDG_DATA_HOME/riddle00
 
+for entry in $DOTFILES_DIR/.local/share/applications/[a-zA-Z]*; do
+    [ -e "$entry" ] &&
+    ln -sf $entry $XDG_DATA_HOME/applications/${entry##*/}
+done
