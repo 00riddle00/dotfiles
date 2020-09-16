616b538e339706725c114e025bf54cdf7fc2de63
Tomas Giedraitis <tomasgiedraitis@gmail.com>
2020-09-16 12:46:57 +0300

    readd emacs config

---
+++ .emacs.d/init.el
@@ -0,0 +1,41 @@
+(windmove-default-keybindings)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+   ["black" "#d55e00" "#009e73" "#f8ec59" "#0072b2" "#cc79a7" "#56b4e9" "white"])
+ '(custom-enabled-themes (quote (deeper-blue)))
+ '(custom-unlispify-menu-entries nil)
+ '(inhibit-startup-screen t)
+ '(menu-bar-mode nil)
+ '(nil nil t)
+ '(package-selected-packages (quote (linum-relative evil)))
+ '(scroll-bar-mode nil)
+ '(tool-bar-mode nil))
+  (setq evil-want-C-u-scroll t )
+  ;(setq linum-relative-current-symbol "")
+  (require 'package)
+  (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+  (package-initialize)
+  (require 'evil)
+  ;(require 'linum-relative)
+  (evil-mode 0)
+  (setq package-archives
+  '(("gnu" . "http://elpa.gnu.org/packages/")
+  ("marmalade" . "http://marmalade-repo.org/packages/")
+  ("melpa" . "http://melpa.milkbox.net/packages/")))
+  (setq apropos-sort-by-scores t)
+  (set-face-attribute 'default nil :height 80)
+  (global-set-key (kbd "C-z") 'evil-local-mode)
+  (global-display-line-numbers-mode)
+  
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
