d2f496af5eae80c0d40556c1689c6a69a02570c5
Tomas Giedraitis <tomasgiedraitis@gmail.com>
2022-04-13 15:51:09 +0300

    Org mode config

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -40,7 +40,7 @@
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/org/")
+(setq org-directory "~/Dropbox/org/")
 
 
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
@@ -74,3 +74,15 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
+
+;; Without wrapping everything in (after! org), my settings might be evaluated
+;; too early, which will result in my settings being overwritten by Doom's
+;; defaults.
+(after! org
+  (setq org-directory "~/Dropbox/org/")
+  (setq org-agenda-files '("~/Dropbox/org/agenda.org"))
+  ;(setq org-log-done 'time)
+  ;(setq org-log-done 'note)
+  (require 'org-bullets)
+  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
+)
--- .doom.d/custom.el
+++ .doom.d/custom.el
@@ -3,7 +3,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-agenda-files '("~/pro/2022/org-mode/todo.org"))
  '(package-selected-packages '(undo-tree)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
