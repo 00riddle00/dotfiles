30af18660842763e25461eed49663e60907ee0db
Tomas Giedraitis <tomasgiedraitis@gmail.com>
2022-04-16 18:18:20 +0300

    Move Doom Emacs configs to .config/ dir instead of HOME

--- .config/doom/custom.el
+++ .config/doom/custom.el
@@ -3,12 +3,14 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("835868dcd17131ba8b9619d14c67c127aa18b90a82438c8613586331129dda63" "f6665ce2f7f56c5ed5d91ed5e7f6acb66ce44d0ef4acfaa3a42c7cfe9e9a9013" default))
  '(package-selected-packages '(undo-tree)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(hl-line ((t (face-attribute 'default :background)))))
 (put 'upcase-region 'disabled nil)
 (put 'downcase-region 'disabled nil)
