7f2c76bbf934c798b599ea91c7fb2e9117ce1002
Tomas Giedraitis <tomasgiedraitis@gmail.com>
2023-01-25 14:05:08 +0200

    Doom Emacs: Load custom theme, remove "custom-set-faces" duplicate entry (!) and remove "package-selected-packages".

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -31,8 +31,11 @@
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
-;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one)
+;; `load-theme' function.
+;; The default theme:
+;;(setq doom-theme 'doom-one)
+;; Custom:
+(setq doom-theme 'atom-one-dark)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
@@ -85,10 +88,6 @@
   ;(setq org-log-done 'note)
   (require 'org-bullets)
   (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
-)
-
-(custom-set-faces
-  '(hl-line ((t (face-attribute 'default :background))))
 )
 
 (global-set-key (kbd "C-s") 'save-buffer)
