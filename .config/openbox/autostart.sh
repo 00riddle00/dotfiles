275b3c0208906a21238a729486827f4eaaa0d5e3
Tomas Giedraitis <tomasgiedraitis@gmail.com>
2024-04-05 22:46:49 +0300

    Update Openbox config files to make them more similar to files in /etc/xdg/openbox. Openbox version 3.6.1

--- .config/openbox/autostart.sh
+++ .config/openbox/autostart.sh
@@ -1,4 +1,14 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
+# vim:ft=bash:tw=79
+#==============================================================================
+# Author: 00riddle00 (Tomas Giedraitis)
+# Date:   2024-04-05 21:14:30 EEST
+# Path:   ~/.config/openbox/autostart.sh
+# URL:    https://github.com/00riddle00/dotfiles
+#==============================================================================
+#*
+# These user-specific things are run when an Openbox X Session is started.
+#**
 
 xsetroot -solid "#073542" &
 #conky -c $XDG_CONFIG_HOME/conky/stats_blue > /dev/null 2>&1 &
