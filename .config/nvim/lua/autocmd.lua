4e91e23200652ff35ba23ffd1ef648cd6fb174c9
Tomas Giedraitis <tomasgiedraitis@gmail.com>
2024-05-08 11:08:55 +0300

    Close a tab/nvim when nvim-tree is the last window.

--- .config/nvim/lua/autocmd.lua
+++ .config/nvim/lua/autocmd.lua
@@ -54,12 +54,12 @@ autocmd("syntax", {
 })
 
 -- Automatically remove trailing whitespaces unless file is blacklisted.
-autocmd("BufWritePre", {
-  callback = function()
-    General.Preserve(function() vim.cmd("%s/\\s\\+$//e") end)
-  end,
-  group = general
-})
+--autocmd("BufWritePre", {
+  --callback = function()
+    --General.Preserve(function() vim.cmd("%s/\\s\\+$//e") end)
+  --end,
+  --group = general
+--})
 
 -- Ensure directory structure exists when opening a new file.
 autocmd("BufNewFile", {
@@ -154,10 +154,39 @@ autocmd("QuickFixCmdPost", {
 -- they sometimes receive an error "No folds found", or that treesitter
 -- highlighting does not apply. A workaround for this is to set the folding
 -- options in an autocmd:
-vim.api.nvim_create_autocmd({"BufEnter","BufAdd","BufNew","BufNewFile","BufWinEnter"}, {
+autocmd({"BufEnter","BufAdd","BufNew","BufNewFile","BufWinEnter"}, {
   group = vim.api.nvim_create_augroup("TS_FOLD_WORKAROUND", {}),
   callback = function()
     vim.opt.foldmethod     = "expr"
     vim.opt.foldexpr       = "nvim_treesitter#foldexpr()"
   end
 })
+
+---------------------------------------------
+-- [Plugin] "nvim-tree/nvim-tree.lua"
+---------------------------------------------
+
+-- Close the tab/nvim when nvim-tree is the last window.
+autocmd("QuitPre", {
+  callback = function()
+    local tree_wins = {}
+    local floating_wins = {}
+    local wins = vim.api.nvim_list_wins()
+    for _, w in ipairs(wins) do
+      local bufname = vim.api.nvim_buf_get_name(vim.api.nvim_win_get_buf(w))
+      if bufname:match("NvimTree_") ~= nil then
+        table.insert(tree_wins, w)
+      end
+      if vim.api.nvim_win_get_config(w).relative ~= '' then
+        table.insert(floating_wins, w)
+      end
+    end
+    if 1 == #wins - #floating_wins - #tree_wins then
+      -- Should quit, so we close all invalid windows.
+      for _, w in ipairs(tree_wins) do
+        vim.api.nvim_win_close(w, true)
+      end
+    end
+  end
+})
+
