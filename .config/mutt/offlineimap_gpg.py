9b540b650f7a26b4579d455f74584426ff6e4b70
Tomas Giedraitis <tomasgiedraitis@gmail.com>
2024-07-13 13:19:12 +0300

    Change "User" to "Author" in some dotfiles headers.

--- .config/mutt/offlineimap_gpg.py
+++ .config/mutt/offlineimap_gpg.py
@@ -1,9 +1,9 @@
 #! /usr/bin/env python2
 #------------------------------------------------------------------------------
-# User: 00riddle00 (Tomas Giedraitis)
-# Date: 2020-03-28 23:07:40 EET
-# Path: ~/.config/mutt/offlineimap_gpg.py
-# URL:  https://github.com/00riddle00/dotfiles
+# Author: 00riddle00 (Tomas Giedraitis)
+# Date:   2020-03-28 23:07:40 EET
+# Path:   ~/.config/mutt/offlineimap_gpg.py
+# URL:    https://github.com/00riddle00/dotfiles
 #------------------------------------------------------------------------------
 
 from subprocess import check_output
