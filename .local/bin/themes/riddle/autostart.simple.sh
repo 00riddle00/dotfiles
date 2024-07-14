e0b796b4ad047643fb4337914cc9f492cbb3ea04
Tomas Giedraitis <tomasgiedraitis@gmail.com>
2024-07-14 20:10:32 +0300

    Update Matrix and Riddle themes' startup scripts.

--- .local/bin/themes/riddle/autostart.simple.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-feh  --bg-scale "$CANDY/images/desktop/black_striped.jpg" 
-conky -c $XDG_CONFIG_HOME/conky/stats_blue > /dev/null 2>&1 &
-conky -c $XDG_CONFIG_HOME/conky/clock_blue > /dev/null 2>&1 &
-tint2 &
-urxvt -name t2 &
