db1fd6610f0ee645e15e9957e2ebf2c444a1fc00
Tomas Giedraitis <tomasgiedraitis@gmail.com>
2024-08-08 00:36:09 +0300

    Start tidying up shell functions.

--- .config/zsh/functions.zsh
+++ .config/zsh/functions.zsh
@@ -1,79 +1,143 @@
 #------------------------------------------------------------------------------
 # Author: 00riddle00 (Tomas Giedraitis)
-# Date:   2024-08-07 23:00:29 EEST
+# Date:   2024-08-08 00:35:53 EEST
 # Path:   ~/.config/zsh/functions.zsh
 # URL:    https://github.com/00riddle00/dotfiles
 #------------------------------------------------------------------------------
 
-# Make a temp backup
-bk() { cp "$1" "$1.bak"; }
+# --------------------------------------------
+# Aliases
+# --------------------------------------------
 
-# Make an alias for zsh
-#   ex. usage (called from specific directory):
-#      ma "vedit" "vim $(readlink -f file_to_edit.txt)"
-#      ma "cddir" "cd $(pwd)"
-ma() { echo alias "$1='$2'" >> "$ZDOTDIR/aliases.zsh"; echo "***Alias added***\nalias $1='$2'"; zsh }
-
-# Make an alias for zsh to cd into current dir.
-macd() { echo alias "$1='cd $(pwd)'" >> "$ZDOTDIR/aliases.zsh"; echo "***Alias added***\nalias $1='cd $(pwd)'"; zsh }
-
-# Systemwide functions
-
-## Find command in history
-his() { history | grep "$1"; }
-
-jr() { javac "$1" && java $(echo $1 | sed 's/.java//') $(echo "${@:2}"); }
-
-# Add numbers to non empty lines in a file
-# Usage: num <file>
-num() { nl -s ' ' "$1" > tmp && mv tmp "$1" && sed "s/^[ \t]*//" -i "$1" && cat "$1" | xclip }
-
-# Go to command's flag description in  manpage, ex. `manf grep -r`
-manf () { man "$1" | less -p "^ +$2"; }
-
-# Returns "A" \ "B" (subtracting identical lines)
-a_minus_b() { grep -Fvx -f "$2" "$1" | sort; }
-
-# Removes lines from file "A" which contain string from file "B"
-a_minus_string_in_b() { grep -Fv -f "$2" "$1" | sort; }
-
-a_and_string_in_b() { grep -F -f "$2" "$1" | sort; }
-
-# Returns "A" & "B" (identical lines)
-a_and_b() { comm -12 <( sort "$1" ) <( sort "$2" ) }
-
-# Returns "A" U "B" (union of lines, removes duplicate lines)
-a_union_b() { cat "$1" "$2" | sort -u }
-
-# Find all files from current folder | prints extension of files if any | make a unique sorted list
-ext() { find . -type f | perl -ne 'print $1 if m/\.([^.\/]+)$/' | sort -u; }
-
-# Copy arguments to clipboard
-sel() { echo "$@" | xclip}
-
-get-mp3() { youtube-dl --no-playlist --extract-audio --audio-format mp3 "$1"; }
-
-yy() { name="${1%.asm}"; yasm "$name.asm" -fbin -o "$name.com"; }
-
-# Opens a temporary file with a given extension
-# (default extension: .md)
-# File is opened in /tmp dir, it's name contains a timestamp
-temp() {
-    ext="$1";
-    [[ -z "$1" ]] && ext="md"
-    vim -c "e /tmp/temp_$(date +%F_%H_%M_%S).$ext | :cd %:p:h"
+# Create a Zsh alias
+# Usage e.g.:
+#     ma "vedit" "vim $(readlink -f file_to_edit.txt)"
+#     ma "cdhere" "cd $(pwd)"
+ma() { 
+    echo alias "$1  '$2'" >> "$ZDOTDIR/aliases.zsh"
+    echo "*** Alias added ***"
+    echo "alias $1  '$2'"
+    source "$ZDOTDIR/.zshrc"
 }
 
-# Usage: fields <file>
+# Create a Zsh alias to cd into current directory
+# Usage e.g.:
+#   macd "cdhere"
+macd() { 
+    echo alias "$1='cd $(pwd)'" >> "$ZDOTDIR/aliases.zsh"
+    echo "*** Alias added ***
+    alias $1='cd $(pwd)'"
+    source "$ZDOTDIR/.zshrc"
+}
+
+# --------------------------------------------
+# Backups
+# --------------------------------------------
+
+# Make a backup in the current directory
+# Usage:
+#   bk FILE1|DIR1 [FILE2|DIR2] ...
+bk() {
+    for item in "$@"; do
+        cp -r "$item" "$item.bak"
+    done
+}
+
+# Make a backup both in local disk and remote location
+# Usage:
+#   kk FILE1|DIR1 [FILE2|DIR2] ...
+function kk() {
+    for item in "$@"; do
+        cp -rv "$item" "${HOME}/backups/${item}.$(date +%F_%R_%S).bak"
+        cp -rv "$item" "${DROPBOX}/backup/${item}.$(date +%F_%R_%S).bak"
+    done
+}
+
+# --------------------------------------------
+# Find
+# --------------------------------------------
+
+# List all file extensions in the current directory (recursively)
+# Usage:
+#   ext
+ext() { 
+    find . -type f \
+        | perl -ne 'print $1 if m/\.([^.\/]+)$/' \
+        | sort -u
+}
+
+# TODO update / tidy up
+# Searches hidden files as well
+find.file() {
+    find . -iname "*${1}*" | sort -u
+}
+
+# TODO update / tidy up
+# Does not search hidden files
+find.file.abs() {
+    readlink -f **/** | grep -i "${1}" | sort -u
+}
+
+# TODO update / tidy up
+# Find different file extensions, exluding ./.git, ./venv and ./.idea dirs.
+files-ext() {
+    find . \
+        -path    ./.git \
+        -o -path ./venv  -prune \
+        -o -path ./.idea -prune \
+        -o -type f \
+            | sed -rn 's|.*/[^/]+\.([^/.]+)$|\1|p' \
+            | sort -u
+}
+
+# --------------------------------------------
+# Set operations on files (linewise)
+# --------------------------------------------
+
+# A U B (union of lines, without duplicates)
+# Usage:
+#   a_union_b FILE1 FILE2
+a_union_b() { cat "$1" "$2" | sort -u }
+
+# A & B (intersection of lines)
+# Usage:
+#   a_and_b FILE1 FILE2
+a_and_b() { comm -12 <( sort "$1" ) <( sort "$2" ) }
+
+# Get all lines from A which contain a string from B
+# Usage:
+#   a_and_string_in_b FILE1 FILE2
+a_and_string_in_b() { grep -F -f "$2" "$1" | sort }
+
+# A \ B (subtract lines from A which appear in B)
+# Usage:
+#   a_minus_b FILE1 FILE2
+a_minus_b() { grep -Fvx -f "$2" "$1" | sort }
+
+# Remove lines from A which contain a string from B
+# Usage:
+#   a_minus_string_in_b FILE1 FILE2
+a_minus_string_in_b() { grep -Fv -f "$2" "$1" | sort }
+
+# --------------------------------------------
+# Structured data files
+# --------------------------------------------
+
+# Summary of the number of fields in each line of a TSV file
+# Usage:
+#   fields FILE
 fields () {
     awk -F"\t" '{print NF}' "$1" | uniq -c
 }
 
-# Usage: fields <file>
+# Summary of the number of fields in each line of a CSV file
+# Usage:
+#   fields-csv FILE
 fields-csv () {
     awk -F, '{print NF}' "$1" | uniq -c
 }
 
+# TODO update / tidy up
 # Usage: cols <col_no> <file>
 # $1 - col no
 # $2 - file name
@@ -83,6 +147,7 @@ cols () {
     awk -F$'\t' -f <(echo "$col_no") "$2" | sort | uniq
 }
 
+# TODO update / tidy up
 # Usage: cols <col_no> <file>
 # $1 - col no
 # $2 - file name
@@ -92,6 +157,7 @@ colss () {
     awk -F$'\t' -f <(echo "$col_no") "$2" | sort
 }
 
+# TODO update / tidy up
 # Usage: cols <col_no> <file>
 cols-csv () {
     col_no="\$$1";
@@ -99,6 +165,7 @@ cols-csv () {
     awk -F, -f <(echo "$col_no") "$2" | sort | uniq
 }
 
+# TODO update / tidy up
 # Usage: cols <col_no> <file>
 ncols () {
     col_no="\$$1";
@@ -106,13 +173,22 @@ ncols () {
     awk -F$'\t' -f <(echo "$col_no") "$2" | sort -n | uniq
 }
 
+# TODO update / tidy up
 # Usage: pcols <col_no> <file>
 pcols () {
     col_no="\$$1";
     col_no="{print $col_no}";
-    awk -F$'\t' -f <(echo "$col_no") "$2" | perl -e '@data = map {[split(/\t/,$_)]} <>; print map {join("\t",@$_)} sort {$a->[0] <=> $b->[0]} @data' | uniq
+    awk -F$'\t' -f <(echo "$col_no") "$2" \
+        | perl -e '@data = map {[split(/\t/,$_)]} <>; \
+            print map {join("\t",@$_)} sort {$a->[0] <=> $b->[0]} @data' \
+        | uniq
 }
 
+# --------------------------------------------
+# Timestamps
+# --------------------------------------------
+
+# TODO update / tidy up
 # Convert epoch to human-readable date
 # Usage: epoch-to-date 1643234400
 # Output: 2022-01-27
@@ -120,6 +196,7 @@ epoch-to-date() {
     date  -d @"$1" "+%F"
 }
 
+# TODO update / tidy up
 # Convert epoch to human-readable date
 # Usage: date-to-epoch 2022-01-27
 # Output: 1643234400
@@ -127,134 +204,149 @@ date-to-epoch() {
     date  -d "$1" "+%s"
 }
 
-ddif() {
-    meld \
-        <( cat "$1" | (sed -u 1q; sort -k1 -n) ) \
-        <( cat "$2" | (sed -u 1q; sort -k1 -n) ) \
+# --------------------------------------------
+# Misc
+# --------------------------------------------
+
+# Go to a command's flag description in its manpage
+# Usage e.g.:
+#   manf grep -r
+manf () { man "$1" | less -p "^ +$2"; }
+
+# Edit a new file with a given extension (default is .md) in /tmp
+# Usage: 
+#   temp [EXT]
+temp() {
+    ext="$1"
+    [[ -z "$1" ]] && ext="md"
+    $EDITOR -c "e /tmp/temp_$(date +%F_%H_%M_%S).$ext | :cd %:p:h"
 }
 
+# TODO update / tidy up
 bsc() {
     basename "$1" | xclip
 }
 
+# TODO update / tidy up
 rlc() {
     readlink -f "$1" | xclip
 }
 
-catc() {
-    cat "$1" | xclip
+# TODO update / tidy up
+recent () {
+    history \
+        | grep "$1" \
+        | grep -v "recent $1" \
+        | grep -v "grep $1" \
+        | tail -1
 }
 
-# Searches hidden files as well
-find.file() {
-    find . -iname "*${1}*" | sort -u
-}
-
-# Does not search hidden files
-find.file.abs() {
-    readlink -f **/** | grep -i "${1}" | sort -u
-}
-
-recent ()
-{
-    history | grep "$1" | grep -v "recent $1" | grep -v "grep $1" | tail -1
-}
-
+# TODO update / tidy up
 auf() {
-    ps ax | grep -v grep | grep -i "${1}" | awk '{print $1}'
+    ps ax \
+        | grep -v grep \
+        | grep -i "${1}" \
+        | awk '{print $1}'
 }
 
+# TODO update / tidy up
 svndiff() {
-  sep="\n$(perl -e 'print("@" x 100);')\n"
+    sep="\n$(perl -e 'print("@" x 100);')\n"
 
-  svn diff "$@" \
-  | awk -v sep="$sep" '/^Index: / {print sep} {print}' \
-  | bat --language diff
+    svn diff "$@" \
+        | awk -v sep="$sep" '/^Index: / {print sep} {print}' \
+        | bat --language diff
 }
 
+# TODO update / tidy up
 c() {
-  if [ -z "$1" ]; then
-    cd
-  elif [ "$(find "$1" -maxdepth 1 -type f | head -n 51 | wc -l)" -gt 50 ]; then
-    cd "$1"
-    echo "Large dir"
-  else
-    cd "$1" && ls
-  fi
-}
-
-out() {
     if [ -z "$1" ]; then
-        echo "Usage: out <file>"
+        cd
+    elif [ "$(find "$1" -maxdepth 1 -type f \
+               | head -n 51 \
+               | wc -l)" \
+           -gt 50 ]; then
+        cd "$1"
+        echo "Large dir"
     else
-        $EDITOR "outputs/${1%.com}."*
+        cd "$1" && ls
     fi
 }
 
-sedit() {
-    $EDITOR "$(which "$1")"
-}
-
+# TODO update / tidy up
 tmux-clean() {
     echo "Sucessfully killed unattached Tmux sessions."
     echo "--------------------------------------------"
     echo "Before:"
     tmux ls
-    tmux ls | grep -v attached | cut -d: -f1 | xargs -I{} tmux kill-session -t {}
+    tmux ls \
+        | grep -v attached \
+        | cut -d: -f1 \
+        | xargs -I{} tmux kill-session -t {}
     echo "After:"
     tmux ls
 }
 
-# Find different file extensions, exluding ./.git, ./venv and ./.idea dirs.
-files-ext() {
-    find . -path ./.git -o -path ./venv -prune -o -path ./.idea -prune -o -type f \
-    | sed -rn 's|.*/[^/]+\.([^/.]+)$|\1|p' \
-    | sort -u
-}
-
-function kk() {
-    cp -rv "$1" "${HOME}/backups/${1}.$(date +%F_%R_%S).bak"
-    cp -rv "$1" "${DROPBOX}/backup/${1}.$(date +%F_%R_%S).bak"
-}
-
-open() {
-    vim $(codid2file "$1")
-}
-
-# Copy using fzf
-fcp() { cp -v "$1" "$(awk '{print $2}' ~/.config/directories | fzf | sed "s|~|$HOME|")" ; }
-
-# Edit scripts (using fzf)
-se() { du -a $BIN/* ~/.local/bin/* | awk '{print $2}' | fzf --preview 'bat --color always {}' | xargs -ro $EDITOR ; }
-
-# -----------------------------------------------------------------------------
+# --------------------------------------------
 # FZF functions
-# -----------------------------------------------------------------------------
+# --------------------------------------------
 
-# ---------------------------------------
+# TODO update / tidy up
+# Copy using fzf
+fcp() {
+    cp -v "$1" "$(awk '{print $2}' ~/.config/directories \
+                    | fzf \
+                    | sed "s|~|$HOME|")"
+}
+
+# TODO update / tidy up
+# Edit scripts (using fzf)
+se() {
+    du -a $BIN/* ~/.local/bin/* \
+        | awk '{print $2}' \
+        | fzf --preview 'bat --color always {}' \
+        | xargs -ro $EDITOR
+}
+
+# -----------------------
 # File
-# ---------------------------------------
+# -----------------------
 
+# TODO update / tidy up
 # fe - open file with $EDITOR
 fe() {
-  local file
-  file="$(
-    /usr/bin/fd --type f --follow --exclude .git --max-depth=1 2> /dev/null \
-      | fzf +m --preview "bat --color=always --style=numbers --line-range=:500 {}" --bind 'ctrl-u:preview-page-up,ctrl-d:preview-page-down' --preview-window=right:60%:wrap
-  )" || return
+    local file
+    file="$(
+      /usr/bin/fd --type f --follow --exclude .git --max-depth=1 2> /dev/null \
+      | fzf +m \
+        --preview "bat \
+            --color=always \
+            --style=numbers \
+            --line-range=:500 {}" \
+        --bind 'ctrl-u:preview-page-up,ctrl-d:preview-page-down' \
+        --preview-window=right:60%:wrap
+    )" || return
   $EDITOR "$file" || return
 }
 
+# TODO update / tidy up
 # fea - including hidden files (only in current dir)
 fea() {
   local file
   file="$(
     /usr/bin/fd --type f --hidden --follow --exclude .git --max-depth=1 2> /dev/null \
-      | fzf +m --preview "bat --color=always --style=numbers --line-range=:500 {}" --bind 'ctrl-u:preview-page-up,ctrl-d:preview-page-down' --preview-window=right:60%:wrap
+      | fzf +m \
+        --preview "bat \
+            --color=always \
+            --style=numbers \
+            --line-range=:500 {}"\
+        --bind 'ctrl-u:preview-page-up,ctrl-d:preview-page-down' \
+        --preview-window=right:60%:wrap
   )" || return
   $EDITOR "$file" || return
 }
 
+# TODO update / tidy up
 # fzf.vi - open files in ~/.viminfo
 fzf.vi() {
   local files
@@ -269,20 +361,23 @@ fzf.vi() {
   "${EDITOR:-vim}" "${files/\~/$HOME}"
 }
 
-# ---------------------------------------
+# -----------------------
 # Directory
-# ---------------------------------------
+# -----------------------
 
+# TODO update / tidy up
 # fd - cd to selected directory
 fd() {
   local dir
   dir="$(
-    find "${1:-.}" -path '*/\.*' -prune -o -type d -print 2> /dev/null \
+    find "${1:-.}" -path '*/\.*' -prune \
+        -o -type d -print 2> /dev/null \
       | fzf +m
   )" || return
   cd "$dir" || return
 }
 
+# TODO update / tidy up
 # fda - including hidden directories
 fda() {
   local dir
@@ -293,15 +388,17 @@ fda() {
   cd "$dir" || return
 }
 
-# ---------------------------------------
+# -----------------------
 # History
-# ---------------------------------------
+# -----------------------
 
+# TODO update / tidy up
 # runcmd - utility function used to run the command in the shell
 runcmd() {
   perl -e 'ioctl STDOUT, 0x5412, $_ for split //, <>'
 }
 
+# TODO update / tidy up
 # fh - repeat history
 fh() {
   ([[ -n "$ZSH_NAME" ]] && fc -l 1 || history) \
@@ -310,11 +407,13 @@ fh() {
     | runcmd
 }
 
+# TODO update / tidy up
 # writecmd - utility function used to write the command in the shell
 writecmd() {
   perl -e 'ioctl STDOUT, 0x5412, $_ for split //, do { chomp($_ = <>); $_ }'
 }
 
+# TODO update / tidy up
 # fhe - repeat history edit
 fhe() {
   ([[ -n "$ZSH_NAME" ]] && fc -l 1 || history) \
@@ -323,10 +422,11 @@ fhe() {
     | writecmd
 }
 
-# ---------------------------------------
+# -----------------------
 # PID
-# ---------------------------------------
+# -----------------------
 
+# TODO update / tidy up
 # fkill - kill process
 fkill() {
   local pid
@@ -341,10 +441,11 @@ fkill() {
   kill -"${1:-9}" "$pid"
 }
 
-# ---------------------------------------
+# -----------------------
 # Tmux
-# ---------------------------------------
+# -----------------------
 
+# TODO update / tidy up
 # ftpane - switch pane (@george-b)
 ftpane() {
   local panes
