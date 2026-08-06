022dbade2e80e99a2d0a907141ba17f1d4589379
Tomas Giedraitis <tomasgiedraitis@gmail.com>
2026-08-06 20:46:16 +0200

    Add Zsh directory bookmarks for MSc course folders.

---
+++ .config/zsh/bookmarks.zsh
@@ -0,0 +1,14 @@
+#------------------------------------------------------------------------------
+# Author: 00riddle00 (Tomas Giedraitis)
+# Date:   2026-08-06 21:05:41 CEST
+# Path:   ~/.config/zsh/bookmarks.zsh
+# URL:    https://github.com/00riddle00/dotfiles
+#------------------------------------------------------------------------------
+
+# Directory bookmarks
+#
+# Use hash -d to list all bookmarks
+# Use unhash -d <name> to remove a bookmark
+# Use ~<name> to reference a bookmark
+hash -d ms_fun=~/CS/MS_2_Semester/01_Functional_Data_Analysis
+hash -d ms_bda=~/CS/MS_2_Semester/02_Big_Data_Analysis
