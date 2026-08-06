5a91bf9f3eb956348d6d72d4e3b46a20921d4b74
Tomas Giedraitis <tomasgiedraitis@gmail.com>
2026-08-06 23:30:18 +0200

    Add WezTerm config with TokyoNight Moon theme.

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,26 @@
+-- vim: set ft=lua tw=79 nu ai et ts=2 sw=2:
+-------------------------------------------------------------------------------
+-- Author: 00riddle00 (Tomas Giedraitis)
+-- Date:   2026-08-06 23:29:47 CEST
+-- Path:   ~/.config/wezterm/wezterm.lua
+-- URL:    https://github.com/00riddle00/dotfiles
+-------------------------------------------------------------------------------
+
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices.
+
+-- For example, changing the initial geometry for new windows:
+config.initial_cols = 120
+config.initial_rows = 28
+
+-- or, changing the font size and color scheme.
+config.font_size = 10
+config.color_scheme = 'Tokyo Night Moon'
+
+-- Finally, return the configuration to wezterm:
+return config
