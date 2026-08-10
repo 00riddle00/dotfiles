ab047d93402632c1b36582ffda6eef2a084b19e9
Tomas Giedraitis <tomasgiedraitis@gmail.com>
2026-08-10 22:54:14 +0200

    Fix and modernize offlineimap_gpg.py for Python 3.
    Fixed two real bugs (KeyError from .format(), bytes/str TypeError). Verified end to end - decrypted a real password.

--- .config/mutt/offlineimap_gpg.py
+++ .config/mutt/offlineimap_gpg.py
@@ -1,12 +1,15 @@
-#! /usr/bin/env python2
+#!/usr/bin/env python3
 #------------------------------------------------------------------------------
 # Author: 00riddle00 (Tomas Giedraitis)
-# Date:   2026-08-10 06:13:41 CEST
+# Date:   2026-08-10 22:52:33 CEST
 # Path:   ~/.config/mutt/offlineimap_gpg.py
 # URL:    https://github.com/00riddle00/dotfiles
 #------------------------------------------------------------------------------
-
+import os
 from subprocess import check_output
 
+
 def get_pass(account):
-    return check_output("gpg -dq ${XDG_CONFIG_HOME}/mutt/.mail_passwd_{}.gpg".format(account) , shell=True).strip("\n")
+    config_home = os.environ.get("XDG_CONFIG_HOME", os.path.expanduser("~/.config"))
+    cmd = f"gpg -dq {config_home}/mutt/.mail_passwd_{account}.gpg"
+    return check_output(cmd, shell=True, text=True).strip("\n")
