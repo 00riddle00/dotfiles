63a25f7aaceb557973f20873801091a61876d781
Tomas Giedraitis <tomasgiedraitis@gmail.com>
2026-08-23 20:32:16 +0200

    Configure WezTerm formatting and Alt+V paste
    Add a project-local StyLua configuration and format the existing WezTerm Lua config.
    
    Bind Alt+V to paste from the clipboard.

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,13 +1,13 @@
 -- vim: set ft=lua tw=79 nu ai et ts=2 sw=2:
 -------------------------------------------------------------------------------
 -- Author: 00riddle00 (Tomas Giedraitis)
--- Date:   2026-08-06 23:29:47 CEST
+-- Date:   2026-08-23 20:31:32 CEST
 -- Path:   ~/.config/wezterm/wezterm.lua
 -- URL:    https://github.com/00riddle00/dotfiles
 -------------------------------------------------------------------------------
 
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
@@ -20,7 +20,11 @@ config.initial_rows = 28
 
 -- or, changing the font size and color scheme.
 config.font_size = 10
-config.color_scheme = 'Tokyo Night Moon'
+config.color_scheme = "Tokyo Night Moon"
+
+config.keys = {
+  { key = "v", mods = "ALT", action = wezterm.action.PasteFrom("Clipboard") },
+}
 
 -- Finally, return the configuration to wezterm:
 return config
