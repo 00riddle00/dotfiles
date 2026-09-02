fddb1a3a8d1a2ea218acb518981117701a5ffaa1
Tomas Giedraitis <tomasgiedraitis@gmail.com>
2026-09-02 23:26:01 +0200

    Organize Nvim plugins by category.

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,85 +1,16 @@
 -- vim: set ft=lua tw=79 nu ai et ts=2 sw=2:
 --------------------------------------------------------------------------------
 -- Author: 00riddle00 (Tomas Giedraitis)
--- Date:   2026-09-02 23:18:13 CEST
+-- Date:   2026-09-02 23:21:23 CEST
 -- Path:   ~/.config/nvim/lua/plugins/init.lua
 -- URL:    https://github.com/00riddle00/dotfiles
 --------------------------------------------------------------------------------
 
 return {
-  -- Run the :Mtm command to toggle markdown table mode.
-  {
-    "Kicamon/markdown-table-mode.nvim",
-    config = function()
-      require("markdown-table-mode").setup({
-        filetype = {
-          "*.md",
-          "*.sql",
-        },
-        options = {
-          insert = true, -- when typing "|"
-          insert_leave = true, -- when leaving insert
-          pad_separator_line = true, -- add space in separator line
-          alig_style = "default", -- default, left, center, right
-        },
-      })
-    end,
-  },
-  {
-    "lewis6991/gitsigns.nvim",
-    config = function()
-      require("plugins.gitsigns")
-    end,
-  },
-  --[[
-  -- Left here for testing it
-  {
-    "kdheepak/lazygit.nvim",
-    lazy = true,
-    cmd = {
-      "LazyGit",
-      "LazyGitConfig",
-      "LazyGitCurrentFile",
-      "LazyGitFilter",
-      "LazyGitFilterCurrentFile",
-    },
-    -- optional for floating window border decoration
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-    },
-  },
-  --]]
-  --[[
-  -- Left here for testing it
-  {
-    "NeogitOrg/neogit",
-    dependencies = {
-      "nvim-lua/plenary.nvim",         -- required
-      "sindrets/diffview.nvim",        -- optional - Diff integration
-      -- Only one of these is needed.
-      "nvim-telescope/telescope.nvim", -- optional
-      -- "ibhagwan/fzf-lua",              -- optional
-      -- "echasnovski/mini.pick",         -- optional
-    },
-  },
-  --]]
-  {
-    "sindrets/diffview.nvim",
-    opts = {},
-    config = function()
-      require("diffview").setup({
-        hooks = {
-          diff_buf_read = function()
-            vim.cmd("norm! gg]ckzt") -- Set cursor on the first hunk
-          end,
-          diff_buf_win_enter = function()
-            vim.opt_local.foldlevel = 99
-          end,
-        },
-      })
-    end,
-  },
-  -- UI / Color
+  ------------------------------------------------------------------------------
+  -- UI / Appearance
+  ------------------------------------------------------------------------------
+
   {
     "folke/tokyonight.nvim",
     priority = 1000,
@@ -88,12 +19,44 @@ return {
       vim.cmd([[colorscheme tokyonight]])
     end,
   },
+
   {
-    "m4xshen/hardtime.nvim",
-    enabled = false,
-    dependencies = { "MunifTanjim/nui.nvim" },
-    opts = {},
+    "folke/snacks.nvim",
+    priority = 1000,
+    lazy = false,
+    opts = {
+      notifier = { enabled = true },
+      quickfile = { enabled = true },
+      words = { enabled = true },
+    },
   },
+
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    config = function()
+      require("plugins.ibl")
+    end,
+  },
+
+  {
+    "HiPhish/rainbow-delimiters.nvim",
+    config = function()
+      require("plugins.rainbow_delimiters")
+    end,
+  },
+
+  {
+    "xiyaowong/virtcolumn.nvim",
+    config = function()
+      require("plugins.virtcolumn")
+    end,
+  },
+
+  ------------------------------------------------------------------------------
+  -- Navigation / Files / Symbols
+  ------------------------------------------------------------------------------
+
   {
     "nvim-neo-tree/neo-tree.nvim",
     branch = "v3.x",
@@ -104,16 +67,7 @@ return {
     },
     lazy = false, -- neo-tree will lazily load itself
   },
-  -- install with yarn or npm
-  {
-    "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    build = "cd app && yarn install",
-    init = function()
-      vim.g.mkdp_filetypes = { "markdown" }
-    end,
-    ft = { "markdown" },
-  },
+
   {
     "stevearc/aerial.nvim",
     opts = {
@@ -132,10 +86,10 @@ return {
       --   "Module",
       --   "Method",
       --   "Struct",
-      --   "Variable",    -- 👈 add this!
-      --   "Field",       -- 👈 and this
-      --   "Property",    -- 👈 optional
-      --   "Constant",    -- 👈 optional
+      --   "Variable",
+      --   "Field",
+      --   "Property",
+      --   "Constant",
       -- },
     },
     dependencies = {
@@ -143,7 +97,13 @@ return {
       "nvim-tree/nvim-web-devicons",
     },
   },
-  -- Telescope
+
+  { "mbbill/undotree" },
+
+  ------------------------------------------------------------------------------
+  -- Search / Picker
+  ------------------------------------------------------------------------------
+
   {
     "nvim-telescope/telescope.nvim",
     dependencies = {
@@ -154,14 +114,20 @@ return {
       require("plugins._telescope")
     end,
   },
+
   { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-  -- LSP / Treesitter
+
+  ------------------------------------------------------------------------------
+  -- LSP / Syntax / Formatting
+  ------------------------------------------------------------------------------
+
   {
     "neovim/nvim-lspconfig",
     config = function()
       require("plugins.nvim_lspconfig")
     end,
   },
+
   {
     "nvim-treesitter/nvim-treesitter",
     lazy = false,
@@ -170,8 +136,35 @@ return {
       require("plugins.nvim_treesitter")
     end,
   },
+
+  {
+    "stevearc/conform.nvim",
+    config = function()
+      require("conform").setup({
+        formatters_by_ft = {
+          python = { "ruff_format" },
+          -- python = { "black" },
+          lua = { "stylua" },
+        },
+        -- formatters = {
+        --   black = {
+        --     prepend_args = {
+        --       "--line-length", "88",
+        --       "--preview",
+        --       "--enable-unstable-feature=string_processing",
+        --     },
+        --   },
+        -- },
+      })
+    end,
+  },
+
   { "smjonas/inc-rename.nvim", config = true },
-  -- Text Editing Enhancements
+
+  ------------------------------------------------------------------------------
+  -- Editing
+  ------------------------------------------------------------------------------
+
   {
     "zbirenbaum/copilot.lua",
     cmd = "Copilot",
@@ -180,83 +173,146 @@ return {
       require("plugins.copilot_config")
     end,
   },
+
+  { "windwp/nvim-autopairs", event = "InsertEnter", config = true },
+
+  { "kylechui/nvim-surround", config = true },
+
   { "junegunn/vim-easy-align" },
+
   {
     "houtsnip/vim-emacscommandline",
     config = function()
       require("plugins.vim_emacscommandline")
     end,
   },
+
   {
-    "cameron-wags/rainbow_csv.nvim",
-    config = function()
-      require("plugins.rainbow_csv")
-    end,
+    "m4xshen/hardtime.nvim",
+    enabled = false,
+    dependencies = { "MunifTanjim/nui.nvim" },
+    opts = {},
   },
-  {
-    "HiPhish/rainbow-delimiters.nvim",
-    config = function()
-      require("plugins.rainbow_delimiters")
-    end,
-  },
-  {
-    "lukas-reineke/indent-blankline.nvim",
-    main = "ibl",
-    config = function()
-      require("plugins.ibl")
-    end,
-  },
+
+  ------------------------------------------------------------------------------
   -- Git
+  ------------------------------------------------------------------------------
+
+  {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      require("plugins.gitsigns")
+    end,
+  },
+
   { "tpope/vim-fugitive" },
-  -- Misc
-  { "kylechui/nvim-surround", config = true },
-  { "christoomey/vim-tmux-navigator" },
-  -- { "wakatime/vim-wakatime" },
+
+  {
+    "sindrets/diffview.nvim",
+    opts = {},
+    config = function()
+      require("diffview").setup({
+        hooks = {
+          diff_buf_read = function()
+            vim.cmd("norm! gg]ckzt") -- Set cursor on the first hunk
+          end,
+          diff_buf_win_enter = function()
+            vim.opt_local.foldlevel = 99
+          end,
+        },
+      })
+    end,
+  },
+
+  --[[
+  -- Left here for testing it
+  {
+    "kdheepak/lazygit.nvim",
+    lazy = true,
+    cmd = {
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
+    },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+  },
+  --]]
+
+  --[[
+  -- Left here for testing it
+  {
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim",         -- required
+      "sindrets/diffview.nvim",        -- optional - Diff integration
+      "nvim-telescope/telescope.nvim", -- optional
+      -- "ibhagwan/fzf-lua",           -- optional
+      -- "echasnovski/mini.pick",      -- optional
+    },
+  },
+  --]]
+
+  ------------------------------------------------------------------------------
+  -- Markdown / LaTeX / Structured Text
+  ------------------------------------------------------------------------------
+
+  -- Run the :Mtm command to toggle markdown table mode.
+  {
+    "Kicamon/markdown-table-mode.nvim",
+    config = function()
+      require("markdown-table-mode").setup({
+        filetype = {
+          "*.md",
+          "*.sql",
+        },
+        options = {
+          insert = true, -- when typing "|"
+          insert_leave = true, -- when leaving insert
+          pad_separator_line = true, -- add space in separator line
+          alig_style = "default", -- default, left, center, right
+        },
+      })
+    end,
+  },
+
+  -- Install with yarn or npm.
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = "cd app && yarn install",
+    init = function()
+      vim.g.mkdp_filetypes = { "markdown" }
+    end,
+    ft = { "markdown" },
+  },
+
   {
     "lervag/vimtex",
     config = function()
       require("plugins.vimtex")
     end,
   },
+
   {
-    "stevearc/conform.nvim",
+    "cameron-wags/rainbow_csv.nvim",
     config = function()
-      require("conform").setup({
-        formatters_by_ft = {
-          python = { "ruff_format" },
-          --python = { "black" },
-          lua = { "stylua" },
-        },
-        --formatters = {
-        --  black = {
-        --    prepend_args = {
-        --      "--line-length", "88",
-        --      "--preview",
-        --      "--enable-unstable-feature=string_processing",
-        --    },
-        --  },
-        --},
-      })
+      require("plugins.rainbow_csv")
     end,
   },
-  {
-    "folke/snacks.nvim",
-    priority = 1000,
-    lazy = false,
-    opts = {
-      notifier = { enabled = true },
-      quickfile = { enabled = true },
-      words = { enabled = true },
-    },
-  },
-  { "mbbill/undotree" },
+
+  ------------------------------------------------------------------------------
+  -- Terminal / External Integration
+  ------------------------------------------------------------------------------
+
   { "akinsho/toggleterm.nvim", version = "*", config = true },
-  {
-    "xiyaowong/virtcolumn.nvim",
-    config = function()
-      require("plugins.virtcolumn")
-    end,
-  },
-  { "windwp/nvim-autopairs", event = "InsertEnter", config = true },
+
+  { "christoomey/vim-tmux-navigator" },
+
   { "tridactyl/vim-tridactyl" },
+
+  -- { "wakatime/vim-wakatime" },
 }
