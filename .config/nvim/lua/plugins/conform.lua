25c246d2a6ed915b3b36b4f02e014d9dbef70a5c
Tomas Giedraitis <tomasgiedraitis@gmail.com>
2026-09-18 01:25:34 +0200

    Expand formatter coverage and add LSP fallback for Nvim.

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -1,7 +1,7 @@
 -- vim: set ft=lua tw=79 nu ai et ts=2 sw=2:
 --------------------------------------------------------------------------------
 -- Author: 00riddle00 (Tomas Giedraitis)
--- Date:   2026-09-14 00:42:37 CEST
+-- Date:   2026-09-18 01:25:26 CEST
 -- Path:   ~/.config/nvim/lua/plugins/conform.lua
 -- URL:    https://github.com/00riddle00/dotfiles
 --------------------------------------------------------------------------------
@@ -11,14 +11,29 @@ return {
   config = function()
     require("conform").setup({
       formatters_by_ft = {
-        python = { "ruff_format" },
-        -- python = { "black" },
+        awk = { "gawk" },
+        bash = { "shfmt" },
         javascript = { "prettier" },
         javascriptreact = { "prettier" },
+        json = { "prettier" },
+        lua = { "stylua" },
+        markdown = { "prettier" },
+        python = { "ruff_format" },
+        -- python = { "black" },
+        rust = { "rustfmt" },
+        sh = { "shfmt" },
+        tex = { "latexindent" },
+        toml = { "tombi" },
         typescript = { "prettier" },
         typescriptreact = { "prettier" },
-        lua = { "stylua" },
+        yaml = { "prettier" },
       },
+
+      -- Fall back to LSP formatting when no dedicated formatter is configured.
+      default_format_opts = {
+        lsp_format = "fallback",
+      },
+
       formatters = {
         --[[
         black = {
